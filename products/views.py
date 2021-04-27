43110b48387a0555bb13ee439aa07e745e1208f1
dudeguythethird <keablejoseph@gmail.com>
2021-04-27 12:48:00 +0000

    added sorting and product counts to product page

--- products/views.py
+++ products/views.py
@@ -22,6 +22,9 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+                
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
@@ -38,7 +41,7 @@ def all_products(request):
             if not query:
                 messages.error(request, "You didn't enter andy searc criteria!")
                 return redirect(reverse('products'))
-            
+                
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
